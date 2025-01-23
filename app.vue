8d7f50799858983379317593baa2da44a3070877
sawada1 <khaledsawada1@gmail.com>
2025-01-23 22:26:38 +0200

    test updates

--- app.vue
+++ app.vue
@@ -1,9 +1,25 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div :class="{ dark: isDarkMode1 }">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+  </div>
 </template>
 
 <script setup>
+const isDarkMode1 = ref(false);
+const isDarkMode = ref(false);
 
+onMounted(() => {
+  // Check if the device is in dark mode
+  isDarkMode.value = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  // Optionally listen for changes in the system preference
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+      isDarkMode.value = event.matches;
+    });
+});
 </script>

516b7fbe35055ee869b00c80c4c52c592d53e75b
sawada1 <khaledsawada1@gmail.com>
2025-04-13 23:28:44 +0200

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -161,6 +161,33 @@ src="https://www.facebook.com/tr?id=1214734386356861&ev=PageView&noscript=1"
         `,
           
         },
+        {
+          async: true,
+          innerHTML: `
+!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+  ttq.load('CVRR33JC77U0VMU9BU10');
+  ttq.page();
+}(window, document, 'ttq');
+        `,
+          
+        },
+        {
+          async: true,
+          innerHTML: `
+(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+r.src=n;var u=t.getElementsByTagName(s)[0];
+u.parentNode.insertBefore(r,u);})(window,document,
+'https://sc-static.net/scevent.min.js');
+snaptr('init', 'e0a33dd1-375f-434f-bfdb-031e997a5800', {});
+snaptr('track', 'PAGE_VIEW');
+        `,
+          
+        },
       ],
     },
   },
